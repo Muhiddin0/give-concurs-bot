14d4d530d913c51030220932d5667a15e205ed33
Muhiddinjon-kabraliev <88494561+Muhiddin0@users.noreply.github.com>
2023-04-12 18:38:25 +0500

    Update pul_yechish.py

--- handlers/users/pul_yechish.py
+++ handlers/users/pul_yechish.py
@@ -2,24 +2,15 @@ from aiogram import types
 from loader import dp, bot
 import sqlite3
 
-# db = sqlite3.connect('data/users.db')
-# sql = db.cursor()
+db = sqlite3.connect('data/users.db')
+sql = db.cursor()
 
 
 @dp.callback_query_handler(text_contains="pul_yechish")
 async def callback_handler(query: types.CallbackQuery):
+    
+    sql.execute(f"""SELECT * FROM users WHERE user_id = {query['from']['id']}""")
+    user_data = sql.fetchall()[0]
 
-    await query.message.answer(text="bo'lim tez kunda ishga tushadi")
-
-    # user_id = query.message.from_user.id
-
-    # # user_id
-    # sql.execute(f"""SELECT rowid, * FROM users ORDER BY active_user_count DESC""")
-    # top_users = sql.fetchall()
-
-    # main_text = "❗️Top foydalanuvchilar\n\n"
-    # for i in top_users:
-    #     res_user = await bot.get_chat(chat_id=i[1])
-    #     main_text += f"""{i[0]}) <a href="tg://user?id={res_user.id}">{res_user.first_name}</a> active:{i[2]}, passive:{i[3]}\n"""
-
-    # await query.message.answer(text=main_text)
+    await query.message.answer(text=f"Sizning hissobingizda {user_data[1]} ball bor")
+    
